a1432d70b779705fd7d83625c4a095c63ead5e51
Vorschlag-bit <https://github.com/Vorschlag-bit>
2024-07-12 14:48:39 +0900

    졸리다..

---
+++ chapter04-controll-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/C_while.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.looping;
+
+public class C_while {
+
+    public void testSimpleWhileStatement() {
+        int i = 1;
+        while (i < 10) {
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+}

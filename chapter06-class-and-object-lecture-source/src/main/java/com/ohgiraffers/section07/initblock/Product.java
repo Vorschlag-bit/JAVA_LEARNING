d334b34c4e80941cf17f2af9b46781a662967def
Vorschlag-bit <bustout@naver.com>
2024-07-16 14:46:15 +0900

    comit

--- chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section07/initblock/Product.java
+++ chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section07/initblock/Product.java
@@ -1,18 +1,31 @@
 package com.ohgiraffers.section07.initblock;
 
 public class Product {
-    private String name;
+    private String name = "iphone";
     private int price;
-    private static String brand;
+    private static final String brand;
+
+    {
+        System.out.println("초기화 블럭 생성");
+        name = "sony";
+        brand = "LG";
+    }
+    // 객체와 아무 상관없이 static 영역에 미리 할당되어 있기 때문에, static 생성자로 선언한 건 문제없으나, 일반 생성자로 선언하면 오류가 나온다
+    static {
+        brand = "Cyon";
+    }
 
     public Product() {
+        System.out.println("Product 기본 생성자 호출...");
+        name = "iPhono";
     }
 
     @Override
     public String toString() {
         return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
+                "name='" + this.name + '\'' +
+                ", price=" + this.price +
+                ", brand='" + Product.brand + '\'' +
                 '}';
     }
 }
---
+++ chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/object_array/Application.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section08.object_array;
+
+public class Application {
+}

c77e2ab4f5bfc335bdb2e63387b62ae1f7bdefaa
Vorschlag-bit <bustout@naver.com>
2024-07-17 09:59:36 +0900

    상속에 대해서

--- chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/overloading/parameter/ParameterTest.java
+++ chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/overloading/parameter/ParameterTest.java
@@ -8,6 +8,7 @@ public class ParameterTest {
         num = 10;
         System.out.println("Parameter로 전달받은 값: " + num);
     }
+
     // 배열을 넘기는 것이 아닌 주소값을 넘기는 것이다.
     public void TestPrimitiveTypeArrayParameter(int[] iArr) {
         iArr[0] = 1000;
@@ -18,6 +19,7 @@ public class ParameterTest {
         Holycow.calculateArea();
         Holycow.calculateRound();
     }
+
     // 내가 제공하는 parameter 개수와 상관없이 ...은 그걸 모두 받아 배열로 만든다
     // 그 증거로 toString을 사용하지 않고 그냥 출력하면 주소값이 출력된다
     /* 가변인자를 쓰지 않는 이유: 설계되지 않은 변수를 허용하지 않는 게 Java의 방식 */
@@ -25,3 +27,5 @@ public class ParameterTest {
         System.out.println("str = " + Arrays.toString(etc));
     }
 }
+
+
